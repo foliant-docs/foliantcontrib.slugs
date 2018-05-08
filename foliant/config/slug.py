53bd67193982ecd700f7e2152da490f1e56ac20f
Artemy Lomov <lomov@restream.rt.ru>
2018-05-08 19:33:05 +0300

    Take a list of values from YAML config file.

--- foliant/config/slug.py
+++ foliant/config/slug.py
@@ -1,8 +1,12 @@
 '''
-TODO
+Extension for Foliant to generate slugs
+from arbitrary lists of values.
+
+Resolves ``!slug`` YAML tag in the project config.
+Joins the values of list items into string, and appends
+current date to this string.
 '''
 
-import inspect
 from yaml import BaseLoader, add_constructor, load
 from datetime import date
 from typing import Dict
@@ -25,19 +29,8 @@ class Parser(BaseParser):
             return project_config
 
     def _resolve_slug_tag(self, loader, node) -> str:
-        node_value = node.value
+        components = loader.construct_sequence(node)
 
-        components = []
+        components.append(date.today())
 
-        components.append(node_value.replace(' ', '_'))
-
-        version = self._project_config.get('version')
-
-        if version:
-            components.append(str(version))
-
-        components.append(str(date.today()))
-
-        slug = '-'.join(components)
-
-        return slug
+        return '-'.join(map(lambda component: str(component).replace(' ', '_'), components))
