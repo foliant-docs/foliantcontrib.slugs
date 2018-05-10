1a6063245d56cc28bf38649b8a0a893e6721fe77
Artemy Lomov <lomov@restream.rt.ru>
2018-05-10 14:02:01 +0300

    Add '!date' tag resolving. Remove unnecessary parts of code.

--- foliant/config/slug.py
+++ foliant/config/slug.py
@@ -3,13 +3,13 @@ Extension for Foliant to generate slugs
 from arbitrary lists of values.
 
 Resolves ``!slug`` YAML tag in the project config.
-Joins the values of list items into string, and appends
-current date to this string.
+
+Replaces the list of values after the ``!slug`` tag with
+the string that joins these values using ``-`` delimeter.
+Spaces in values are replaced with underscores.
 '''
 
-from yaml import BaseLoader, add_constructor, load
-from datetime import date
-from typing import Dict
+from yaml import add_constructor, load
 
 from foliant.config.base import BaseParser
 
@@ -18,19 +18,11 @@ class Parser(BaseParser):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self._project_config = self._get_project_config()
-
         add_constructor('!slug', self._resolve_slug_tag)
-
-    def _get_project_config(self) -> Dict:
-        with open(self.config_path) as project_config_file:
-            project_config = {**self._defaults, **load(project_config_file, Loader=BaseLoader)}
-
-            return project_config
 
     def _resolve_slug_tag(self, loader, node) -> str:
         components = loader.construct_sequence(node)
 
-        components.append(date.today())
+        self.logger.debug(f'Resolving !slug tag. Custom slug components: {components}')
 
         return '-'.join(map(lambda component: str(component).replace(' ', '_'), components))
